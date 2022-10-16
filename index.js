cf8645f658af2652ebf9fb0fb5859804f3d0fd90
Fikerus <Fikerus@github.com>
2022-10-16 22:22:42 +0300

    Added a few things, changed a lot of stuff

--- index.js
+++ index.js
@@ -1,74 +1,62 @@
 const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d', {alpha: false})
+const progress = document.getElementById('progress').getContext('2d')
 const input = document.getElementById('input')
 const button = document.getElementById('button')
 const result = document.getElementById('result')
 
 const fontSize = 200
 const font = `${fontSize}px arial`
-ctx.font = font
 const offset = 20
 
-ctx.canvas.width = 500
-ctx.canvas.height = 500
+// TODO:
+// Test button before completed (+) (seems okay)
+// Add progress bar
+// Add filling before (seems impossible)
 
-ctx.fillStyle = 'white'
-ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+progress.canvas.width = 50
+progress.canvas.height = 50
 
-input.addEventListener("keypress", function(event) {
+const offscreen = canvas.transferControlToOffscreen()
+const worker = new Worker('worker.js')
+worker.postMessage({message: 'setup', font: font, offset: offset, canvas: offscreen}, [offscreen])
+
+worker.onmessage = (event) => {
+    const data = event.data
+    if (data.message === 'completed') {
+        result.textContent = `Number of holes: ${data.count}`
+    }
+    if (data.message === 'resize') {
+        progress.canvas.width = data.width
+
+        progress.font = '40px arial'
+        progress.fillStyle = 'black'
+        progress.textAlign = 'center'
+        progress.textBaseline = 'middle'
+        progress.fillText('I will add progress bar in future version, sorry <3', progress.canvas.width / 2, progress.canvas.height / 2)
+    }
+    // if (data.message === 'progress') {
+    //     console.log(data.percent)
+    //     progress.fillRect(0, 0, data.percent * progress.canvas.width, progress.canvas.height)
+    // }
+}
+
+input.oninput = () => {
+    let text = input.value
+
+    worker.postMessage({message: 'text', text: text})
+}
+
+input.oninput()
+
+input.onkeypress = (event) => {
     if (event.key === "Enter") {
       event.preventDefault()
       button.click()
     }
-})
-
-function getImage(ctx, width, height){
-    const rawdata = ctx.getImageData(0, 0, width, height).data
-    // console.log(rawdata)
-    const data = []
-    for (let y=0; y<height; ++y){
-        const row = []
-        for (let x=0; x<width; ++x){
-            row.push(rawdata[(y*width + x) * 4] < 128 ? 0 : 255)
-        }
-        data.push(row)
-    }
-    return data
-}
-// Old and bad function
-function getPixels(ctx, width, height){
-    const data = []
-    for (let y=0; y<height; ++y){
-        const row = []
-        for (let x=0; x<width; ++x){
-            let pixel = ctx.getImageData(x, y, 1, 1).data[0]
-            row.push(pixel < 128 ? 0 : 255)
-        }
-        data.push(row)
-    }
-    return data
 }
 
-button.addEventListener('click', updateButton)
+button.onclick = () => {
+    input.oninput()
 
-function updateButton(){
-    ctx.font = font
-    let text = input.value.split("").join(String.fromCharCode(8202))
-    const textWidth = ctx.measureText(text).width
-
-    ctx.canvas.width = Math.ceil(textWidth) + offset
-
-    ctx.font = font
-
-    const width = ctx.canvas.width
-    const height = ctx.canvas.height
-
-    ctx.fillStyle = "white"
-    ctx.fillRect(0, 0, width, height)
-    ctx.fillStyle = "black"
-    ctx.fillText(text, offset/2, height/2 + 50)
-
-    const data = getImage(ctx, width, height)
-
-    result.textContent = countHoles(data)
+    worker.postMessage({message: 'count'})
 }
---
+++ worker.js
@@ -0,0 +1,120 @@
+const w = 400
+const h = 400
+
+let ctx = null
+let font = null
+let offset = null
+
+onmessage = (event) => {
+    const data = event.data
+    const message = data.message
+
+    if (message === 'setup') {
+        ctx = data.canvas.getContext('2d')
+        ctx.canvas.width = w
+        ctx.canvas.height = h
+        font = data.font
+        offset = data.offset
+    }
+    if (message === 'text') {
+        ctx.font = font
+        let text = data.text.split('').join(String.fromCharCode(8202))
+        const textWidth = ctx.measureText(text).width
+
+        ctx.canvas.width = Math.ceil(textWidth) + offset
+        ctx.canvas.height = h
+
+        ctx.font = font
+
+        const width = ctx.canvas.width
+        const height = ctx.canvas.height
+
+        ctx.fillStyle = 'white'
+        ctx.fillRect(0, 0, width, height)
+        ctx.fillStyle = 'black'
+        ctx.textBaseline = 'middle'
+        ctx.fillText(text, offset/2, height/2)
+
+        postMessage({message: 'resize', width: width})
+    }
+    if (message === 'count') {
+        // console.time('test')
+        const pixels = getPixels(ctx, ctx.canvas.width, ctx.canvas.height)
+        let result = countHoles(pixels)
+        postMessage({message: 'completed', count: result})
+        // console.timeEnd('test')
+    }
+}
+
+function getPixels(ctx, width, height){
+    const rawdata = ctx.getImageData(0, 0, width, height).data
+    const data = []
+    for (let y=0; y<height; ++y){
+        const row = []
+        for (let x=0; x<width; ++x){
+            row.push(rawdata[(y*width + x) * 4] < 128 ? 0 : 255)
+        }
+        data.push(row)
+    }
+    return data
+}
+
+function countHoles(pixels){
+    const visited = pixels.map(arr => arr.map(() => false))
+    let count = 0
+    const stack = []
+    const areas = []
+
+    function DFS(x, y){
+        if (y < 0 || y >= pixels.length || x < 0 || x >= pixels[0].length || visited[y][x] || pixels[y][x] === 0){
+            return
+        }
+
+        if (areas.length)
+            areas[areas.length-1] += 1
+
+        visited[y][x] = true
+
+        if (paint){
+            ctx.fillStyle = "lime"
+            ctx.fillRect( x, y, 1, 1 )
+        }
+
+        stack.push(() => DFS(x + 1, y))
+        stack.push(() => DFS(x, y + 1))
+        stack.push(() => DFS(x - 1, y))
+        stack.push(() => DFS(x, y - 1))
+    }
+    function execDFS(x, y){
+        DFS(x, y)
+        let task
+        while (stack.length){
+            task = stack.shift()
+            task()
+        }
+    }
+
+    let paint = false
+
+    execDFS(0, 0) // Don't count and paint the background
+
+    paint = true
+
+    for (let y=0; y<pixels.length; ++y){
+        for (let x=0; x<pixels[0].length; ++x){
+            if(!visited[y][x]){
+                if (pixels[y][x]===255){
+                    areas.push(0)
+                    count += 1
+                }
+                execDFS(x, y)
+            }
+        }
+    }
+    areas.map(area => {
+        if (area < 4){
+            count -= 1
+        }
+    })
+    return count
+}
